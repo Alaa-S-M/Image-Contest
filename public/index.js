2b2c1806de36b7fd770a65344b0c011d72a8e557
ibk <ekittani@gmail.com>
2021-01-24 01:23:17 +0200

    images

--- public/index.js
+++ public/index.js
@@ -30,16 +30,45 @@ function renderImages(images) {
         html += `<div class='rate-image'> 
                 <img src=${image.src}>
                 <div class="rating">
-                <span onclick>☆</span>
-                <span>☆</span>
-                <span>☆</span>
-                <span>☆</span>
-                <span>☆</span>
+                    <span id="1" onclick="handleSatrs(event)">☆</span>
+                    <span id="2" onclick="handleSatrs(event)">☆</span>
+                    <span id="3" onclick="handleSatrs(event)">☆</span>
+                    <span id="4" onclick="handleSatrs(event)">☆</span>
+                    <span id="5" onclick="handleSatrs(event)">☆</span>
                 </div>
-                </div>`;
+            </div>`;
     });
 
     root.innerHTML = html;
+}
+
+function handleSatrs(e) {
+    let stars = [...e.target.closest('.rating').children];
+    console.log(stars)
+    //console.log('sdfdsfsdfsdfdsfdsfdsfsdf')
+    //console.log(e.target.id);
+    stars.forEach((elm, index) => {
+        if (elm.id >= e.target.id) {
+            elm.classList.add("gold");
+        }     
+    })
+    console.log(e.target.closest('.rate-image').firstElementChild.src+ ' === src ==== ');
+    console.log(parseInt(e.target.id) + ' ====id ==== ');
+    updateScore(parseInt(e.target.id),e.target.closest('.rate-image').firstElementChild.src);
+}
+
+function updateScore(score,src){
+    !!src && fetch('/update-score', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }, body: JSON.stringify({ score, src })
+    })
+        .then(r => r.json())
+        .then(data => {
+            console.log(data);
+        });
+
 }
 
 function sortImages() {
--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 
 const images = [
-    { score: '0', src: 'https://joombig.com/demo-extensions1/images/gallery_slider/Swan_large.jpg' },
+    { score: '0', src: 'https://joombig.com/demo-extensions1/images/gallery_slider/Swan_large.jpg' }
 
 ];
 
@@ -26,7 +26,21 @@ app.get('/get-images', (req,res) => {
     res.send(images);
 });
 
+app.post('/update-score', (req, res) => {
+    const { score, src } = req.body;
+    console.log(src);
+    const index = images.findIndex(image => image.src === src);
+    if (index > -1) {
+        images[index].score = score;
+        console.log('Updated');
+    }
+    console.log(images[index].score)
+    res.send({ ok: true });
+
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log('listening', port);
 });
+
